6c5b11606c8ffb30ff0382c0de887fa90d3f096f
Mendyvandervliet <mendyvandervliet@live.nl>
2016-03-17 15:41:24 +0000

    rnw improved + shorter data loading function

--- R/KNMI_loading.R
+++ R/KNMI_loading.R
@@ -1,31 +1,13 @@
 # Data loading
 
 
-getdata_uur <- read.table("http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi", header=TRUE, quote="\"", sep=",",
-                          stns=c(235,260,280,320,380),vars=c("STN", "YYYYMMDD", "HH", "DD", "FH", "FX", "T", "TD", "DR", "RH", "U"),
-                          byear=1958,bmonth=1,bday=1,eyear=2015,emonth=12,eday=31)
-
-
-# Create correct data format and units
-loaddata<- function(url,stns,vars,datetime){
-  tmp <- read.table(url, header=TRUE, quote="\"",
-                    stns=stns,vars=vars, col.names=vars,
-                    byear=1958,bmonth=1,bday=1,eyear=2015,emonth=12,eday=31)
-  a_B<-as.PCICt(strptime(tmp$YYYYMMDD,"%Y%m%d"),cal="gregorian")
-  Date <- as.Date(as.POSIXlt(a_B))
-  tmp$date <- as.POSIXct(paste(Date,as.character(tmp$HH)), format="%Y-%m- %d %H")
-  tmp$Day <- format(tmp$Date,format="%d")
-  tmp$month <- tmp$date$mon+1
-  tmp$Year <- as.integer(format(tmp$Date,format="%Y"))
-
-  tmp$RH <- tmp$RH * 0.1            #Convert unit= 0.1 m back to unit=1 mm
-  tmp$T <- tmp$T * 0.1
-  tmp$RH[tmp$RH == -0.1] <- 0.0     #All negative values(RH<0.05) become 0
+# Function to load data table with correct units
+loaddata<- function(file){
+  tmp <- fread("./inst/extData/KNMI_rainhr.txt", sep=",",col.names=c("STN", "YYYYMMDD", "HH", "DD", "T", "TD", "DR", "RH", "U", "WW"))
+  tmp[, Date := as.Date(as.POSIXlt(as.PCICt(strptime(YYYYMMDD,"%Y%m%d"),cal="gregorian")))]
+  tmp[, date := as.POSIXct(paste(Date,as.character(HH)), format="%Y-%m- %d %H")]
+  tmp[, ':='(Year= year(Date), Month= month(Date), Day= mday(Date))]
+  tmp[, ':='(RH = RH * 0.1, T = T * 0.1, TD = TD * 0.1, DR = DR * 0.1)] # RH in mm, T and Td in degrees Celsius, DR in hrs
+  tmp[RH == -0.1] <- tmp[RH == -0.1][,RH := 0]                          # All negative values(RH<0.05) become 0
 }
-
-
-loaddata(url="http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi",
-         stns=c(235,260,280,320,380), vars= c("STN", "YYYYMMDD", "HH", "DD", "FH", "FX", "T", "TD", "DR", "RH", "U"),)
-
-
 
