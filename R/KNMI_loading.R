2c28c668bbf48cf712ca99bb37f139ea2d9abe67
Mendyvandervliet <mendyvandervliet@live.nl>
2016-03-17 17:37:42 +0000

    Changed loading function (still not working in Rnw file)

--- R/KNMI_loading.R
+++ R/KNMI_loading.R
@@ -1,13 +1,17 @@
 # Data loading
 
 
-# Function to load data table with correct units
+#' Function to load data table with correct units
+#' @export
 loaddata<- function(file){
-  tmp <- fread("./inst/extData/KNMI_rainhr.txt", sep=",",col.names=c("STN", "YYYYMMDD", "HH", "DD", "T", "TD", "DR", "RH", "U", "WW"))
+  tmp <- fread(file, sep=",")
+  setnames(tmp,c("STN", "YYYYMMDD", "HH", "DD", "T", "TD", "DR", "RH", "U", "WW"))
+  #tmp <- data.table(tmp)
   tmp[, Date := as.Date(as.POSIXlt(as.PCICt(strptime(YYYYMMDD,"%Y%m%d"),cal="gregorian")))]
   tmp[, date := as.POSIXct(paste(Date,as.character(HH)), format="%Y-%m- %d %H")]
   tmp[, ':='(Year= year(Date), Month= month(Date), Day= mday(Date))]
   tmp[, ':='(RH = RH * 0.1, T = T * 0.1, TD = TD * 0.1, DR = DR * 0.1)] # RH in mm, T and Td in degrees Celsius, DR in hrs
-  tmp[RH == -0.1] <- tmp[RH == -0.1][,RH := 0]                          # All negative values(RH<0.05) become 0
+  tmp[RH == -0.1] <- tmp[RH == -0.1][,RH := 0] # All negative values(RH<0.05) become 0
+  return(tmp)
 }
 
