84febd1c66ee235b041afadc047385435a81242e
Mendyvandervliet <mendyvandervliet@live.nl>
2016-03-18 10:04:45 +0000

    Changed loading function with extra step for Data, and imported data.table (as loaddata is depend on data.table)

--- R/KNMI_loading.R
+++ R/KNMI_loading.R
@@ -2,16 +2,22 @@
 
 
 #' Function to load data table with correct units
+#' @import data.table
+
 #' @export
-loaddata<- function(file){
-  tmp <- fread(file, sep=",")
+loaddata <- function(file){
+  tmp <- fread(file)
   setnames(tmp,c("STN", "YYYYMMDD", "HH", "DD", "T", "TD", "DR", "RH", "U", "WW"))
   #tmp <- data.table(tmp)
-  tmp[, Date := as.Date(as.POSIXlt(as.PCICt(strptime(YYYYMMDD,"%Y%m%d"),cal="gregorian")))]
+  Date <- as.PCICt(strptime(tmp$YYYYMMDD,"%Y%m%d"),cal="gregorian")
+  #tmp[, Date := as.Date(as.POSIXlt(as.PCICt(strptime(YYYYMMDD,"%Y%m%d"),cal="gregorian")))]
+  tmp[, Date := as.Date(as.POSIXlt(Date))]
   tmp[, date := as.POSIXct(paste(Date,as.character(HH)), format="%Y-%m- %d %H")]
   tmp[, ':='(Year= year(Date), Month= month(Date), Day= mday(Date))]
   tmp[, ':='(RH = RH * 0.1, T = T * 0.1, TD = TD * 0.1, DR = DR * 0.1)] # RH in mm, T and Td in degrees Celsius, DR in hrs
   tmp[RH == -0.1] <- tmp[RH == -0.1][,RH := 0] # All negative values(RH<0.05) become 0
   return(tmp)
 }
+
+
 
