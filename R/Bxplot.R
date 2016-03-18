30cc7732c639f96ce157a0619ae1e77f5cdf8b04
Mendyvandervliet <mendyvandervliet@live.nl>
2016-03-18 14:04:55 +0000

    Changed boxplot loading function; added levels in stn, to have plots in gg plot with a grouped x axis instead of continious axis (in stn nr). Change height labels boxplot

---
+++ R/Bxplot.R
@@ -0,0 +1,20 @@
+# Boxplot data
+#
+# Function to subset a data file containing only boxplot data
+# As data is lognormal, we use logaritmic plotting. Therefore all data which value = 0 is omitted.
+
+# Load dependent package
+#' @import data.table
+#'
+# Get boxplot data
+#' @export
+bxdata <- function(file) {
+  bx <- file[RH>0]
+  bx<- within(bx,
+                  STN <- factor(STN))
+  bx[, ':='(Qm = mean(RH)), by=STN]
+  bx[, ':='(Q50 = quantile(RH, probs=c(0.50))), by=STN]
+  bx[, ':='(Q99 = quantile(RH, probs=c(0.99)), Q99.9 = quantile(RH, probs=c(0.999))), by=STN]
+  bx <- subset(bx, select=c("STN", "RH", "Qm", "Q99", "Q99.9"))
+  return(bx)
+}
