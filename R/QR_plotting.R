aad2d5c11614f4ae972b24920ebc8aaf5fcb5dd4
Mendyvandervliet <mendyvandervliet@live.nl>
2016-03-31 14:33:05 +0200

    Improved figure (faster construction time) by only plotting points relevant to the 99.9 QR fit

--- R/QR_plotting.R
+++ R/QR_plotting.R
@@ -5,15 +5,15 @@
 #' @import ggplot2
 #'
 #' @export
-QR_plotting <- function(data,title,ymax){
-  ggplot(data[RH > quantile(RH,probs=c(0.50))], aes(x = Year)) +        # data
-    geom_point(aes(y=RH),color="gray",size=0.3)+
+QR_plotting <- function(data,title,ymax=15){
+  ggplot(data, aes(x = Year)) +        # data
+    geom_point(data=data[RH >= quantile(RH,probs=0.999)],aes(y=RH),color="gray",size=0.3)+
     geom_line(aes(y=m50), color="black") +
     geom_line(aes(y=m75), color="purple") +
     geom_line(aes(y=m90), color="blue") +
     geom_line(aes(y=m95), color="green") +
     geom_line(aes(y=m99), color="orange") +
-    geom_line(aes(y=m99.9), color="red") +
+    geom_line(aes(y=m999), color="red") +
     #color="Points", scale_color_manual(values=c("Points"="gray","50%"="black", "75%"="purple", "90%"="blue" ,"95%"="green","99%"="orange","99.9%"="red" )) +
     ggtitle(title) + # title
     ylim(0,ymax) +
