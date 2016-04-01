51be715672cf83eae3d876718ec674e9f51d0f33
Mendyvandervliet <mendyvandervliet@live.nl>
2016-04-01 18:22:36 +0200

    add area calculation to function

--- R/MC_coefperm.R
+++ R/MC_coefperm.R
@@ -9,5 +9,10 @@ MC_coefperm <- function(x, y, tau, N=9999, plot=TRUE){
   reps <- replicate(N, round(as.numeric(coef(rq(sample(y) ~ x, tau=tau))[2]),digits=7))
   obs <- round(as.numeric(coef(rq(y ~ x, tau=tau))[2]),digits=7)
   p <- (1+sum(obs > reps))/(N+1)   #p <- mean(reps > obs)
-  return(list(obs=obs,reps=reps,p=p))
+  mean <- mean(reps)
+  sd <- sd(reps)
+  area <- pnorm(max(reps), mean, sd) - pnorm(0, mean, sd)
+  tmp <- data.table(reps,obs,tau,p,area)
+  #return(list(obs=obs,reps=reps,p=p))
+  return(tmp)
 }
