b83acb1a63cd4b512d807220b2ea904e5dcc7ccb
Mendyvandervliet <mendyvandervliet@live.nl>
2016-04-05 12:17:11 +0200

    Remove mean (is not needed as data is already counted per year)

--- R/QR_freqall.R
+++ R/QR_freqall.R
@@ -14,8 +14,5 @@ QR_freqall <- function(data){
   tmp[, f50 := as.numeric(fitted.values(rq(f ~ Year, tau =0.5))),by=STN]
   tmp[, f75 := as.numeric(fitted.values(rq(f ~ Year, tau =0.75))),by=STN]
   tmp[, f90 := as.numeric(fitted.values(rq(f ~ Year, tau =0.90))),by=STN]
-  # reduce computational cost, mean GR fitted values per year
-  tmp[, ":="(m50 = -1, m75 = -1, m90 = -1, m95 = -1, m99 = -1, m999 = -1)]
-  tmp[, ":="(m50=mean(f50),m75=mean(f75),m90=mean(f90),m95=mean(f95),m99=mean(f99),m999=mean(f999)), by=list(Year,STN)]
   return(tmp)
 }
