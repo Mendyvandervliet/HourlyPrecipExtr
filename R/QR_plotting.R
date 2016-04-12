97067a4a060af59e6452364cc8817523b01901a5
Mendyvandervliet <mendyvandervliet@live.nl>
2016-04-12 17:13:29 +0200

    Improved function (possibility to plot individual ggplot with legend)

--- R/QR_plotting.R
+++ R/QR_plotting.R
@@ -5,18 +5,31 @@
 #' @import ggplot2
 #'
 #' @export
-QR_plotting <- function(data,title){
+QR_plotting <- function(data,title,individual=FALSE){
+  if(individual==FALSE){
   ggplot(data, aes(x = Year)) +        # data
-    geom_point(data=data[RH > quantile(RH,probs=0.999)],aes(y=RH),color="gray",size=0.3)+
-    geom_line(aes(y=m50), color="black") +
-    geom_line(aes(y=m75), color="purple") +
-    geom_line(aes(y=m90), color="blue") +
-    geom_line(aes(y=m95), color="green") +
-    geom_line(aes(y=m99), color="orange") +
-    geom_line(aes(y=m999), color="red") +
-    #color="Points", scale_color_manual(values=c("Points"="gray","50%"="black", "75%"="purple", "90%"="blue" ,"95%"="green","99%"="orange","99.9%"="red" )) +
+    #geom_point(data=data[RH > quantile(RH,probs=0.999)],aes(y=RH),color="gray",size=0.3)+
+    #geom_line(aes(y=m50), color="black") +
+    #geom_line(aes(y=m75), color="purple") +
+    geom_line(aes(y=m90,colour="blue")) +
+    geom_line(aes(y=m95, colour="green")) +
+    geom_line(aes(y=m99, colour="orange")) +
+    geom_line(aes(y=m999, colour="red")) +
     ggtitle(title) + # title
     #ylim(0,ymax) +
     xlab("Time") + ylab("I (mm/hr)")+ # x and y-axis label
     theme_bw()
+  } else{
+    ggplot(data, aes(x = Year)) +        # data
+    geom_line(aes(y=m90,colour="blue")) +
+    geom_line(aes(y=m95, colour="green")) +
+    geom_line(aes(y=m99, colour="orange")) +
+    geom_line(aes(y=m999, colour="red")) +
+    scale_color_manual(name = 'Quantiles',values=c("blue","green","orange","red" ),
+                       labels = c('90%','95%','99%','99.9%')) +
+    ggtitle(title) + # title
+    xlab("Time") + ylab("I (mm/hr)")+ # x and y-axis label
+    theme_bw()
+  }
+
 }
