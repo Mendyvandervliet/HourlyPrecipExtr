e36e8758b194277dc0b9ad34a98e4d1a14171928
Mendyvandervliet <mendyvandervliet@live.nl>
2016-04-15 18:25:27 +0200

    Changed color

--- R/QR_freqplotting.R
+++ R/QR_freqplotting.R
@@ -9,7 +9,7 @@ QR_freqplotting <- function(data,title,ymin,ymax){
   ggplot(data, aes(x = Year)) +        # data
     geom_point(data=data,aes(y=f),color="gray",size=0.3)+
     geom_smooth(aes(y=f),method=lm, color="black")+
-    geom_line(aes(y=f25), color="black") +
+    geom_line(aes(y=f25), color="purple") +
     geom_line(aes(y=f50), color="blue") +
     geom_line(aes(y=f75), color="green") +
     geom_line(aes(y=f90), color="orange") +
