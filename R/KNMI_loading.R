77e7735e5bc1549a21196f6937eb2fbdd610e8dd
Mendyvandervliet <mendyvandervliet@live.nl>
2016-05-11 17:52:32 +0200

    Changed function name to name of script Addited further  data formatting (which previously was in rnw file) in function Ignored .RDataTmp

--- R/KNMI_loading.R
+++ R/KNMI_loading.R
@@ -5,7 +5,7 @@
 #' @import data.table
 
 #' @export
-loaddata <- function(file){
+KNMI_loading <- function(file){
   tmp <- fread(file)
   setnames(tmp,c("STN", "YYYYMMDD", "HH", "DD", "T", "TD", "DR", "RH", "U", "WW"))
   #tmp <- data.table(tmp)
@@ -14,8 +14,17 @@ loaddata <- function(file){
   tmp[, Date := as.Date(as.POSIXlt(Date))]
   tmp[, date := as.POSIXct(paste(Date,as.character(HH)), format="%Y-%m- %d %H")]
   tmp[, ':='(Year= year(Date), Month= month(Date), Day= mday(Date))]
+  # Correct units
   tmp[, ':='(RH = RH * 0.1, T = T * 0.1, TD = TD * 0.1, DR = DR * 0.1)] # RH in mm, T and Td in degrees Celsius, DR in hrs
-  tmp[RH == -0.1] <- tmp[RH == -0.1][,RH := 0] # All negative values(RH<0.05) become 0
+  # Generate a column indicating season
+  tmp[,Season := 0]
+  tmp <- within(tmp, Season[(Month == 12) | (Month == 1)| (Month == 2)] <- "Winter")
+  tmp <- within(tmp, Season[(Month == 3) | (Month == 4)| (Month == 5)] <- "Spring")
+  tmp <- within(tmp, Season[(Month == 6) | (Month == 7)| (Month == 8)] <- "Summer")
+  tmp <- within(tmp, Season[(Month == 9) | (Month == 10)| (Month == 11)] <- "Autumn")
+  # Generate a column Intensity
+  tmp[, I := (RH/DR), by=date]
+  tmp <- within(tmp, I[DR==0] <- 0)[RH == -0.1] <- tmp[RH == -0.1][,RH := 0] # All negative values(RH<0.05) become 0
   return(tmp)
 }
 
