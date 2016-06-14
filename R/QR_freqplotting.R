5e60c505d1bd6012cc46da8fa48f237a5841bf58
Mendyvandervliet <mendyvandervliet@live.nl>
2016-06-14 17:53:54 +0200

    Little change in QR_freqplotting (if else loop)

--- R/QR_freqplotting.R
+++ R/QR_freqplotting.R
@@ -7,19 +7,19 @@
 #' @export
 QR_freqplotting <- function(data,title,ymin,ymax,Year=TRUE){
   if(Year==TRUE){
-  ggplot(data, aes(x = Year)) +        # data
-    geom_point(data=data,aes(y=f),color="gray",size=0.3)+
-    geom_line(aes(y=f25), color="purple") +
-    geom_line(aes(y=f50), color="blue") +
-    geom_line(aes(y=f75), color="green") +
-    geom_smooth(aes(y=f),method=lm, color="black")+
-    ggtitle(title) + # title
-    ylim(c(ymin,ymax)) +
-    annotate("text",label=paste("Coef=",round(coef(lm(f ~ Year,data))[[2]],digits=2)),x=Inf,y=Inf, hjust=1.25,vjust=2) +
-    xlab("Time") + ylab("F (counts/yr)")+ # x and y-axis label
-    theme_bw()
+    ggplot(data, aes(x = Year)) +        # data
+      geom_point(data=data,aes(y=f),color="gray",size=0.3)+
+      geom_line(aes(y=f25), color="purple") +
+      geom_line(aes(y=f50), color="blue") +
+      geom_line(aes(y=f75), color="green") +
+      geom_smooth(aes(y=f),method=lm, color="black")+
+      ggtitle(title) + # title
+      ylim(c(ymin,ymax)) +
+      annotate("text",label=paste("Coef=",round(coef(lm(f ~ Year,data))[[2]],digits=2)),x=Inf,y=Inf, hjust=1.25,vjust=2) +
+      xlab("Time") + ylab("F (counts/yr)")+ # x and y-axis label
+      theme_bw()
   }
-  if(Year==FALSE){
+  else{
     ggplot(data, aes(x = d2)) +        # data
       #geom_point(data=data,aes(y=f),color="gray",size=0.3)+
       geom_line(aes(y=f25), color="purple") +
